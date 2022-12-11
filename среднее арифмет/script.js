983a286e333dc2ec9a1bcb0033f8c34a362e63da
serpsergej <serpsergej08@gmail.com>
2022-12-11 11:46:03 +0200

    first commit

---
+++ среднее арифмет/script.js
@@ -0,0 +1,7 @@
+let a = +prompt('');
+let b = +prompt('');
+let c = +prompt('');
+const d = 3
+alert((a + b + c) / d);
+
+
---
+++ часы секунды/script.js
@@ -0,0 +1,4 @@
+
+const Hour = prompt('what time is it now?');
+const Second = Hour * 3600;
+alert(' часы '+Hour+' секунды '+Second+' ');
